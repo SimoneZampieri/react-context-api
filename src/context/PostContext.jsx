e0aa6e1b9b6d9fb4d3eae6582db73d29bb893ad9
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-27 15:41:30 +0100

    [update] added logic in context

--- src/context/PostContext.jsx
+++ src/context/PostContext.jsx
@@ -1,2 +1,15 @@
 import { useState, createContext, useContext } from "react";
 import axios from "axios";
+
+const GlobalContext = createContext();
+
+const GlobalProvider = ({ children }) => {
+  const baseApiUrl = "http://localhost:3000/posts";
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = () => {
+    axios.get(baseApiUrl).then((res) => {
+      setPosts(res.data);
+    });
+  };
+};
