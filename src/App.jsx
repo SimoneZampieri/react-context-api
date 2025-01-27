9b4c70c02053a697d717a549371cb41956554cf7
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-27 15:54:04 +0100

    [update] created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <></>;
-}
+import { PostProvider } from "./context/PostContext";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+    <>
+      <PostProvider>
+        <Header />
+        <Main />
+      </PostProvider>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Lista dei Miei Post</h1>
+    </header>
+  );
+};
+
+export default Header;
